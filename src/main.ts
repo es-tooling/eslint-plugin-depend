b068cb65e920b1677fb34bfaee1dd20974ea924f
Thomas Broyer <t.broyer@ltgt.net>
2025-04-04 16:42:39 +0200

    Add plugin default export (#44)
    * feat: add a default export for the plugin object
    
    This allows associating the plugin with the 'depend' namespace in
    flat config; otherwise ESLint would error out because the plugin
    instances in the user's configuration file and in the 'recommended'
    config aren't the same, while both tentatively associated to the
    same namespace.
    
    This should allow using eslint-plugin-depend from the command line
    with `eslint --plugin depend --rule 'depend/ban-dependencies: error'`
    
    * feat: add the configs to the exported plugin object
    
    This allows referencing the config in string form in a flat
    config `extends` with the `defineConfig` helper:
    
        plugins: { depend },
        extends: [ 'depend/flat/recommended' ],
    
    * feat: add metadata to the plugin object

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,27 @@ import type {Rule, ESLint} from 'eslint';
 import {configFactory as configRecommended} from './configs/recommended.js';
 import {config as configLegacyRecommended} from './configs/legacy-recommended.js';
 import {rule as banDependencies} from './rules/ban-dependencies.js';
+import {readFileSync} from 'node:fs';
+import path from 'node:path';
+
+const {name, version} = JSON.parse(
+  readFileSync(path.resolve(__dirname, '../package.json'), 'utf8')
+);
 
 export const rules: Record<string, Rule.RuleModule> = {
   'ban-dependencies': banDependencies
 };
 
-const plugin: ESLint.Plugin = {rules};
+const plugin: ESLint.Plugin = {
+  meta: {name, version},
+  rules
+};
 
 export const configs = {
   recommended: configLegacyRecommended,
   'flat/recommended': configRecommended(plugin)
 };
+
+plugin.configs = configs;
+
+export default plugin;
